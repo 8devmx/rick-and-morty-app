842ef9fbef92ad862428614e7723f7d9be80379e
Abraham Pech <abraham_16_69@hotmail.com>
2026-08-08 08:50:26 -0500

    Eliminar carpeta de equipo

--- Kungfundidos/js/main.js
+++
@@ -1,6 +0,0 @@
-import fetchData from './api.js'
-import renderAllCharacters from './render.js'
-
-const apiUrl = 'https://rickandmortyapi.com/api/character'
-const response = await fetchData(apiUrl)
-renderAllCharacters(response.results)
